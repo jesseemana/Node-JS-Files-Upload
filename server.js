5a5b7f86e68cf1a5af5cc3d407c59a6013c52383
Jesse Emana <jesseemana@gmail.com>
2023-02-17 16:36:53 +0200

    middleware files added in server

--- server.js
+++ server.js
@@ -3,6 +3,10 @@ const fileUpload = require( 'express-fileupload' );
 const path = require( 'path' );
 require( 'dotenv' ).config();
 
+const fileExtLimiter = require( './middleware/fileExtLimiter' );
+const fileSizeLimiter = require( './middleware/fileSizeLimit' );
+const filesPayloadExists = require( './middleware/filesPayloadExists' );
+
 
 const PORT = process.env.PORT;
 const hostname = '127.0.0.1';
@@ -13,12 +17,16 @@ app.get( '/', ( req, res ) => {
     res.sendFile( path.join( __dirname, 'index.html' ) );
 } );
 
-app.post( '/upload', fileUpload( { createParentPath: true } ), ( req, res ) => {
-    const files = req.files;
-    console.log( files );
+app.post( '/upload', fileUpload( { createParentPath: true } ),
+    filesPayloadExists,
+    fileExtLimiter( [ '.png', '.jpg', '.jpeg' ] ),
+    fileSizeLimiter,
+    ( req, res ) => {
+        const files = req.files;
+        console.log( files );
 
-    return res.json( { status: 'logged', message: 'logged' } );
-} );
+        return res.json( { status: 'logged', message: 'logged' } );
+    } );
 
 app.listen( PORT, hostname, () => {
     console.log( `Server started on PORT: http://${ hostname }:${ PORT }` );
